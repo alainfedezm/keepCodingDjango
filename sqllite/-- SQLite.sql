4040f80209b621818768fb496aac52889fa7fbba
Alain Fernandez <alainfedez@gmail.com>
2022-05-20 11:13:43 +0200

    Subiendo archivos app KeepCoding

---
+++ sqllite/-- SQLite.sql
@@ -0,0 +1,4 @@
+-- SQLite
+SELECT * from appKeepCoding_curso;
+--INSERT INTO appKeepCoding_curso(id,nombre,camada) VALUES(3,"HTML +CSS", "23600");
+https://github.com/alainfedezm/ProyectoKeepCoding.git
